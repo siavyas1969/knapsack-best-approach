f4de54f4e143f9c0e1d448e51e1e03bb12337847
Aladeen <68794318+singhrahulism@users.noreply.github.com>
2020-10-02 00:24:30 +0530

    Update knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -1,30 +1,20 @@
 def knapSack(W, wt, val, n): 
-  
-    # Base Case 
-    if n == 0 or W == 0 : 
-        return 0
-  
-    # If weight of the nth item is  
-    # more than Knapsack of capacity W,  
-    # then this item cannot be included  
-    # in the optimal solution 
-    if (wt[n-1] > W): 
-        return knapSack(W, wt, val, n-1) 
-  
-    # return the maximum of two cases: 
-    # (1) nth item included 
-    # (2) not included 
-    else: 
-        return max( 
-            val[n-1] + knapSack( 
-                W-wt[n-1], wt, val, n-1),  
-                knapSack(W, wt, val, n-1)) 
-  
-# end of function knapSack 
-  
-# To test above function 
+	K = [[0 for x in range(W + 1)] for x in range(n + 1)] 
+
+	for i in range(n + 1): 
+		for w in range(W + 1): 
+			if i == 0 or w == 0: 
+				K[i][w] = 0
+			elif wt[i-1] <= w: 
+				K[i][w] = max(val[i-1] 
++ K[i-1][w-wt[i-1]], K[i-1][w]) 
+			else: 
+				K[i][w] = K[i-1][w] 
+
+	return K[n][W] 
+
 val = [60, 100, 120] 
 wt = [10, 20, 30] 
 W = 50
 n = len(val) 
-print knapSack(W, wt, val, n) 
+print(knapSack(W, wt, val, n)) 
